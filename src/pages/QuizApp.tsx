c1854abd40ebdca6d8c69fb11f8e68721cce69f6
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-20 06:59:11 +0800

    Add playing state in rendering

--- src/pages/QuizApp.tsx
+++ src/pages/QuizApp.tsx
@@ -5,15 +5,20 @@ import { renderGame } from "../engines/renderGame";
 
 export default function QuizApp() {
     const [gameState, setGameState] = useState<GameState>("start");
+    const [answer, setAnswer] = useState<string>("");
 
     function startGame() {
         setGameState("playing");
     }
 
+    function checkAnswer() {
+        console.log("entered!");
+    }
+
     return (
         <>
             <main>
-                {renderGame(gameState, startGame)}
+                {renderGame(gameState, startGame, answer, setAnswer, checkAnswer)}
             </main>
         </>
     )
