f92fc0bbe422a9a01b774d5bcda3b2c7baae4ff2
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-20 05:36:54 +0800

    Initialize gameState

--- src/pages/QuizApp.tsx
+++ src/pages/QuizApp.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
+import type { GameState } from "../types/game"
+
 export default function QuizApp() {
+    const [gameState, setGameState] = useState<GameState>("start");
+
     return (
         <>
             
