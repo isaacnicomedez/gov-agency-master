49790e8a04745ed0793f88c68c62ad67664eafe8
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-20 06:59:51 +0800

    Render playing state

--- src/engines/renderGame.tsx
+++ src/engines/renderGame.tsx
@@ -1,10 +1,21 @@
 import type { GameState } from "../types/game";
 import StartScreen from "../components/StartScreen";
+import QuestionCard from "../components/Questioncard";
+import type { Dispatch, SetStateAction } from "react";
 
-export function renderGame(gameState: GameState, startGame: () => void) {
+export function renderGame(
+        gameState: GameState, 
+        startGame: () => void, 
+        answer: string,
+        setAnswer: Dispatch<SetStateAction<string>>, 
+        checkAnswer: () => void) 
+    {
     switch(gameState) {
         case "start": 
             return <StartScreen onStart={startGame} />;
+        case "playing":
+            return <QuestionCard answer={answer} setAnswer={setAnswer} checkAnswer={checkAnswer}
+        />
         default:
             return null;
     }
