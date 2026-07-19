4f80c98d6f996403609f0d16cf0f77a73c5e27d8
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-19 23:42:19 +0800

    Apply normalizer to compare answer and currentAgency

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { agencies } from "./data/agencies";
+import { normalize } from "./utils/normalize";
 
 function App() {
   const [currentAgency, setCurrentAgency] = useState(() => {
@@ -10,6 +11,10 @@ function App() {
   const [agencyPool, setAgencyPool] = useState([...agencies]);
 
   const [answer, setAnswer] = useState("");
+
+  if (normalize(answer) === normalize(currentAgency.fullName)) {
+    console.log("correct!");
+  }
 
   return (
     <>
