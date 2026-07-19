3d7e9dc36625098bb6013a22d9b32a940642606a
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-19 22:57:07 +0800

    Add agencies data

---
+++ src/data/agencies.ts
@@ -0,0 +1,304 @@
+export const agencies = [
+  {
+    abbreviation: "DepEd",
+    fullName: "Department of Education",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DOH",
+    fullName: "Department of Health",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DFA",
+    fullName: "Department of Foreign Affairs",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DOLE",
+    fullName: "Department of Labor and Employment",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DSWD",
+    fullName: "Department of Social Welfare and Development",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DILG",
+    fullName: "Department of the Interior and Local Government",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DPWH",
+    fullName: "Department of Public Works and Highways",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "DOTr",
+    fullName: "Department of Transportation",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "BIR",
+    fullName: "Bureau of Internal Revenue",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "LTO",
+    fullName: "Land Transportation Office",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "PNP",
+    fullName: "Philippine National Police",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "NBI",
+    fullName: "National Bureau of Investigation",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "SSS",
+    fullName: "Social Security System",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "PhilHealth",
+    fullName: "Philippine Health Insurance Corporation",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "PAGASA",
+    fullName: "Philippine Atmospheric, Geophysical and Astronomical Services Administration",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "TESDA",
+    fullName: "Technical Education and Skills Development Authority",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "CHED",
+    fullName: "Commission on Higher Education",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "BFP",
+    fullName: "Bureau of Fire Protection",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "BJMP",
+    fullName: "Bureau of Jail Management and Penology",
+    difficulty: "easy",
+  },
+  {
+    abbreviation: "AFP",
+    fullName: "Armed Forces of the Philippines",
+    difficulty: "easy",
+  },
+
+  {
+    abbreviation: "BOC",
+    fullName: "Bureau of Customs",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "BI",
+    fullName: "Bureau of Immigration",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "CAAP",
+    fullName: "Civil Aviation Authority of the Philippines",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "MARINA",
+    fullName: "Maritime Industry Authority",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "MMDA",
+    fullName: "Metropolitan Manila Development Authority",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "PEZA",
+    fullName: "Philippine Economic Zone Authority",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "SEC",
+    fullName: "Securities and Exchange Commission",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "GSIS",
+    fullName: "Government Service Insurance System",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DBM",
+    fullName: "Department of Budget and Management",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DAR",
+    fullName: "Department of Agrarian Reform",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DA",
+    fullName: "Department of Agriculture",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DENR",
+    fullName: "Department of Environment and Natural Resources",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DICT",
+    fullName: "Department of Information and Communications Technology",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DMW",
+    fullName: "Department of Migrant Workers",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DOST",
+    fullName: "Department of Science and Technology",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "DOT",
+    fullName: "Department of Tourism",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "PSA",
+    fullName: "Philippine Statistics Authority",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "PCG",
+    fullName: "Philippine Coast Guard",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "PCSO",
+    fullName: "Philippine Charity Sweepstakes Office",
+    difficulty: "medium",
+  },
+  {
+    abbreviation: "PDEA",
+    fullName: "Philippine Drug Enforcement Agency",
+    difficulty: "medium",
+  },
+
+  {
+    abbreviation: "NAMRIA",
+    fullName: "National Mapping and Resource Information Authority",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NEDA",
+    fullName: "National Economic and Development Authority",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NICA",
+    fullName: "National Intelligence Coordinating Agency",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NTC",
+    fullName: "National Telecommunications Commission",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NPC",
+    fullName: "National Privacy Commission",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NFA",
+    fullName: "National Food Authority",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NEA",
+    fullName: "National Electrification Administration",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NIA",
+    fullName: "National Irrigation Administration",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NCIP",
+    fullName: "National Commission on Indigenous Peoples",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "NCMF",
+    fullName: "National Commission on Muslim Filipinos",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "LWUA",
+    fullName: "Local Water Utilities Administration",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "TIEZA",
+    fullName: "Tourism Infrastructure and Enterprise Zone Authority",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "PNRI",
+    fullName: "Philippine Nuclear Research Institute",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "PHIVOLCS",
+    fullName: "Philippine Institute of Volcanology and Seismology",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "MGB",
+    fullName: "Mines and Geosciences Bureau",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "PCA",
+    fullName: "Philippine Coconut Authority",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "BFAR",
+    fullName: "Bureau of Fisheries and Aquatic Resources",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "BAI",
+    fullName: "Bureau of Animal Industry",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "BPI",
+    fullName: "Bureau of Plant Industry",
+    difficulty: "hard",
+  },
+  {
+    abbreviation: "BQ",
+    fullName: "Bureau of Quarantine",
+    difficulty: "hard",
+  },
+];
