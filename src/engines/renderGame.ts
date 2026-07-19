1161f3bdac3dda5eeffafda2f0f9cead050b1675
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-20 06:07:56 +0800

    Rename ts file to tsx

--- src/engines/renderGame.ts
+++
@@ -1,8 +0,0 @@
-import type { GameState } from "../types/game";
-
-export function renderGame(gameState: GameState) {
-    switch(gameState) {
-        case "start": 
-            return <StartScreen />;
-    }
-}
