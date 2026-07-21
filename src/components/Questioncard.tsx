c4bfd88efdf304ffe8653e5df5c499f8c333267c
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-22 06:25:23 +0800

    Rename setter props

--- src/components/Questioncard.tsx
+++ src/components/Questioncard.tsx
@@ -1,19 +1,24 @@
 import { handleKeyDown } from "../engines/handleKeyDown"
 import type { QuestionCardProps } from "../types/props"
+import { useRef } from "react"
 
 export default function QuestionCard({
-    answer, 
-    setAnswer, 
-    checkAnswer,
+    answer,
+    currentAgency,
+    onAnswerChange, 
+    onSubmit,
     }: QuestionCardProps) {
+
+    const inputRef = useRef<HTMLInputElement>(null);
     return (
         <>
-            <p>{}</p>
+            <p>{currentAgency?.abbreviation}</p>
             <input 
+                ref={inputRef}
                 type="text" 
                 value={answer}
-                onChange={e => setAnswer(e.target.value)}
-                onKeyDown={e => handleKeyDown({e, checkAnswer})}
+                onChange={e => onAnswerChange(e.target.value)}
+                onKeyDown={e => handleKeyDown({e, onSubmit})}
             />
         </>
     )
--- src/engines/handleKeyDown.ts
+++ src/engines/handleKeyDown.ts
@@ -1,5 +1,5 @@
 import type { handleKeyDownProps } from "../types/props";
 
-export function handleKeyDown({e, checkAnswer}: handleKeyDownProps) {
-    if (e.key === "Enter") checkAnswer();
+export function handleKeyDown({e, onSubmit}: handleKeyDownProps) {
+    if (e.key === "Enter") onSubmit();
 }
