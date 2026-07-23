aef34ba819f1fe4df481a2164c16855a44df9407
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-23 13:27:38 +0800

    Add shape for statsState

--- src/types/game.ts
+++ src/types/game.ts
@@ -4,3 +4,16 @@ export type GameState =
     | "correct"
     | "wrong"
     | "finished"
+
+export interface Stats {
+    score: number;
+
+    correct: {
+        easy: number;
+        medium: number;
+        hard: number;
+    }
+
+    startedAt: number,
+    finishedAt: number,
+}
