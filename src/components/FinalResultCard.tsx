1c7a16bab566223e33456f31ab98eb4b79419825
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-23 13:48:20 +0800

    Modify UI

--- src/components/FinalResultCard.tsx
+++ src/components/FinalResultCard.tsx
@@ -3,7 +3,15 @@ import type { FinalResultProps} from "../types/props";
 export default function ResultCard({stats, total, accuracy}: FinalResultProps) {
     return (
         <section>
-            
+            <h2>Results:</h2>
+            <p>Easy: {stats.correct.easy}</p>
+            <p>Medium: {stats.correct.medium}</p>
+            <p>Hard: {stats.correct.hard}</p>
+
+            <p>Total: {total}</p>
+            <p>Accuracy: {accuracy}%</p>
+
+            <p>Score: {stats.score}</p>
         </section>
     )
 }
