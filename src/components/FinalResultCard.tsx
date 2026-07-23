0651216ef5300e21d64cae368f8658373aa94e34
Isaac Nicomedez <isaacnicomedez1@gmail.com>
2026-07-23 14:16:12 +0800

    Move toFixed to UI
    To preserve numerative  value

--- src/components/FinalResultCard.tsx
+++ src/components/FinalResultCard.tsx
@@ -9,7 +9,7 @@ export default function ResultCard({stats, total, accuracy, time}: FinalResultPr
             <p>Hard: {stats.correct.hard}</p>
 
             <p>Total: {total}</p>
-            <p>Accuracy: {accuracy}%</p>
+            <p>Accuracy: {accuracy.toFixed(2)}%</p>
 
             <p>Time: {time.minutes > 0 && `${time.minutes}m`} {time.seconds}s</p>
 
